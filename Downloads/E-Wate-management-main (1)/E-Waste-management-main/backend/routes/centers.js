e7eb0ea1021122425f69506328dc372c2c356f83
priyanshu sahay <119564402+Priyanshusahay12222301@users.noreply.github.com>
2025-07-16 00:51:48 +0530

    Delete Downloads/E-Wate-management-main (1)/E-Waste-management-main directory

--- Downloads/E-Wate-management-main (1)/E-Waste-management-main/backend/routes/centers.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { centers } = require('../data');
-const router = express.Router();
-
-// GET /api/centers - return all centers
-router.get('/', (req, res) => res.json(centers));
-
-module.exports = router; 
